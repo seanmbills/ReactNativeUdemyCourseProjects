841c16ec6afefad2d8321e05e19c3a4bce289091
Sean Bills <seanmichaelbills@gmail.com>
2019-08-14 14:06:02 -0400

    updated HomeScreen to include buttons to direct/navigate users to the other screens using buttons

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello, world!</Text>;
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello, world!</Text>
+      <Button 
+        title = "Go to Components Demo"
+        onPress = {() => props.navigation.navigate("Components")}
+      />
+      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
+    textAlign:"center",
   }
 });
 
