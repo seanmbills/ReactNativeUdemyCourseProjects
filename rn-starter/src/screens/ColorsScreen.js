3e1650162c8fe8487ba9aa7a9a3a9fb1f2e059a5
Sean Bills <seanmichaelbills@gmail.com>
2019-08-14 16:00:03 -0400

    updated colorsScreen to use state to maintain an array of the colors we generate in order to display all at once

--- rn-starter/src/screens/ColorsScreen.js
+++ rn-starter/src/screens/ColorsScreen.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import {Button, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Button, View, StyleSheet, FlatList} from 'react-native';
 
 const ColorsScreen = () => {
+    const [colors, setColors] = useState([]);
+
     return (
         <View>
-            <Button title = "Add a Color" />
+            <Button title = "Add a Color" onPress = { () => {
+                setColors([...colors, randomRGB()]);
+            }}/>
+            
+            <FlatList data = {colors} renderItem = {({item}) => {
+                    return <View style = {{height: 100, width: 100, backgroundColor: item}} />
+                }}
+                keyExtractor = {(item) => item}
+            />
         </View>
     );
 };
+
+const randomRGB = () => {
+    const red = Math.ceil(Math.random() * 256) - 1;
+    const green = Math.ceil(Math.random() * 256) - 1;
+    const blue = Math.ceil(Math.random() * 256) - 1;
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
 const styles = StyleSheet.create({});
 
