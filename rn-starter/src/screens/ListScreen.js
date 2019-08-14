969600460b4c575610f7abb27ba6c20d8bab7250
Sean Bills <seanmichaelbills@gmail.com>
2019-08-14 13:04:11 -0400

    created listScreen object and updated app.js to use it

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const ListScreen = () => {
+    return <Text>List Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
