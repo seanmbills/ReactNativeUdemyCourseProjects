41c3b4099fadfe43a88266f3c27f820d7edce6d2
Sean Bills <seanmichaelbills@gmail.com>
2019-08-14 13:45:59 -0400

    updated list screen to include additional friend information (age) and to have app.js display the home screen as the default

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -26,15 +26,27 @@ const ListScreen = () => {
         { name: 'Friend 9'},
     ];
 
+    const friendsWithAges = [
+        { name: 'Friend 1', age: 1},
+        { name: 'Friend 2', age: 2},
+        { name: 'Friend 3', age: 3},
+        { name: 'Friend 4', age: 4},
+        { name: 'Friend 5', age: 5},
+        { name: 'Friend 6', age: 6},
+        { name: 'Friend 7', age: 7},
+        { name: 'Friend 8', age: 8},
+        { name: 'Friend 9', age: 9},
+    ];
+
 
     return (
         <FlatList
-            horizontal
-            showsHorizontalScrollIndicator={false}
+            // horizontal
+            // showsHorizontalScrollIndicator={false}
             keyExtractor = {(friend) => friend.name}
-            data = {friends}
+            data = {friendsWithAges}
             renderItem = {({item}) => {
-                return <Text style = {styles.lineItem}>{item.name}</Text>
+                return <Text style = {styles.lineItem}>Name:{item.name} - Age:{item.age}</Text>
             }}
         />
     );
