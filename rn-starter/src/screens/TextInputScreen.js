a8b9bd79c67e7fd83d4e8bc966d98426c4450453
Sean Bills <seanmichaelbills@gmail.com>
2019-08-14 19:06:40 -0400

    minor changes to make textinputscreen use a "password"

--- rn-starter/src/screens/TextInputScreen.js
+++ rn-starter/src/screens/TextInputScreen.js
@@ -2,18 +2,22 @@ import React, {useState} from 'react'
 import {View, Text, StyleSheet, TextInput} from 'react-native'
 
 const TextInputScreen = () => {
-    const [name, setName] = useState("")
-    
+    // const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
     return (
         <View>
-            <Text style={{fontSize: 30, textAlign:"center"}}>Enter your name:</Text>
+            {/* <Text style={{fontSize: 30, textAlign:"center"}}>Enter your name:</Text> */}
+            <Text style={{fontSize: 30, textAlign:"center"}}>Enter your password:</Text>
             <TextInput style={styles.inputBox}
                 autoCapitalize="none"
                 autoCorrect={false}
-                value = {name}
-                onChangeText = {(newText) => setName(newText)}
+                // value = {name}
+                value = {password}
+                // onChangeText = {(newText) => setName(newText)}
+                onChangeText = {(newPass) => setPassword(newPass)}
             />
-            <Text style={{fontSize:20, textAlign:"center"}}>Hello there, {name}!</Text>
+            {/* <Text style={{fontSize:20, textAlign:"center"}}>Hello there, {name}!</Text> */}
+            { password.length < 5 ? <Text>Please enter a password longer than 5 characters.</Text> : null}
         </View>
     );
 }
