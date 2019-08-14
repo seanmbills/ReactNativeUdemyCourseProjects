78c019be585c728e50ad213cb95d6ea6e214a0c2
Sean Bills <seanmichaelbills@gmail.com>
2019-08-14 14:10:21 -0400

    updated HomeScreen to use mmore compact props.navigation call

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({navigation}) => {
   return (
     <View>
       <Text style={styles.text}>Hello, world!</Text>
       <Button 
         title = "Go to Components Demo"
-        onPress = {() => props.navigation.navigate("Components")}
+        onPress = {() => navigation.navigate("Components")}
       />
       {/* <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity> */}
-      <Button title = "Go to List Demo" onPress = {() => props.navigation.navigate("List")}/>
+      <Button title = "Go to List Demo" onPress = {() => navigation.navigate("List")}/>
     </View>
   );
 };
