921e342b27847b900887029e90fe44c496c05272
Sean Bills <seanmichaelbills@gmail.com>
2019-08-15 11:37:38 -0400

    minor changes to boxscreen and gitignore changes

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -3,12 +3,29 @@ import {Text, View, StyleSheet} from 'react-native'
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style = {styles.viewStyle}>
+            <Text style = {styles.textStyle}>Child 1</Text>
+            <Text style = {styles.textStyle}>Child 2</Text>
+            <Text style = {styles.textStyle}>Child 3</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 2,
+        borderColor: 'black',
+
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        // marginVertical: 20,
+        // marginHorizontal: 20,
+        // margin: 20,
+
+    },
+
+})
 
 export default BoxScreen
