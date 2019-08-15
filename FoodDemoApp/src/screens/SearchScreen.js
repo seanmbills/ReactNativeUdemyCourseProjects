4db4c987260e5d8d2405f1f24db8568012c318be
Sean Bills <seanmichaelbills@gmail.com>
2019-08-15 14:52:44 -0400

    finished setting up yelp api successfully

--- FoodDemoApp/src/screens/SearchScreen.js
+++ FoodDemoApp/src/screens/SearchScreen.js
@@ -1,18 +1,31 @@
 import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp'
+
 
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState("")
+    const [searchResults, setResults] = useState([])
 
+    const searchYelpApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'atlanta'
+            }
+        });
+        setResults(response.data.businesses)
+    }    
 
     return (
         <View>
             <SearchBar searchTerm={searchTerm}
-                onTermChange={newTerm => setSearchTerm(newTerm)}
-                onSearchSubmit={() => console.log("search submitted")}
+                onTermChange={setSearchTerm}
+                onSearchSubmit={searchYelpApi}
             />
-            <Text>{searchTerm}</Text>
+            <Text>We have found {searchResults.length} results!</Text>
         </View>
     )
 }
