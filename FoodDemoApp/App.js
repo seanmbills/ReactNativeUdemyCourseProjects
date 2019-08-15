967ee0114c410faf895d604d52204f3d20a5ccb6
Sean Bills <seanmichaelbills@gmail.com>
2019-08-15 16:15:51 -0400

    creating new screen to show details of an individual result item

--- FoodDemoApp/App.js
+++ FoodDemoApp/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {createStackNavigator, createAppContainer } from 'react-navigation'
 import SearchScreen from './src/screens/SearchScreen';
+import ResultScreen from './src/screens/ResultScreen';
 
 const navigator = createStackNavigator({
     Search: SearchScreen,
-
+    Result: ResultScreen,
+    
 }, {
     initialRouteName: "Search",
     defaultNavigationOptions: {
---
+++ FoodDemoApp/src/screens/ResultScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const ResultScreen = () => {
+    return (
+        <View>
+            <Text>
+                Result Screen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default ResultScreen
