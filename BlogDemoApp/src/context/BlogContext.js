a7e8858f68f48e9b59e807a01345494fdbfd0559
Sean Bills <seanmichaelbills@gmail.com>
2019-08-15 17:58:06 -0400

    prelim data update with button to add new blog post titles

--- BlogDemoApp/src/context/BlogContext.js
+++ BlogDemoApp/src/context/BlogContext.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({children}) => {
+    const [blogPosts, setPosts] = useState([])
+
+    const addBlogPost = () => {
+        setPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+    }
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider 
+            value={{data: blogPosts, 
+                addBlogPost
+        }}>
             {children}
         </BlogContext.Provider>
     )
 }
+
+export default BlogContext
