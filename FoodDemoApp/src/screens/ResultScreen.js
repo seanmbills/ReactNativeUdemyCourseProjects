7f4d6fe374a82df0ef1ecd5aec9d8fa93d9ccccf
Sean Bills <seanmichaelbills@gmail.com>
2019-08-15 16:42:57 -0400

    display image results in individual result screen view

--- FoodDemoApp/src/screens/ResultScreen.js
+++ FoodDemoApp/src/screens/ResultScreen.js
@@ -1,18 +1,48 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, Text, StyleSheet, FlatList, Image} from 'react-native'
+import yelp from '../api/yelp'
 
 const ResultScreen = ({navigation}) => {
     const id = navigation.getParam('id')
-    console.log(id)
+    const [result, setResult] = useState(null)
+
+    console.log(result)
+
+    const getBusinessInfo = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data)
+    }
+
+    useEffect(() => {
+        getBusinessInfo(id)
+    }, [])
+
+    if (!result) {
+        return null
+    }
+
     return (
         <View>
-            <Text>
-                Result Screen
-            </Text>
+            {/* <Text>{result.name}</Text> */}
+            <FlatList data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({item}) => {
+                    return (
+                        <Image style={styles.locationImages} source={{uri: item}}/> 
+                    )
+                }}
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    locationImages: {
+        height: 250,
+        width: 250,
+        borderRadius: 10,
+
+    }
+})
 
 export default ResultScreen
