722c245b97522c54defe141e95487fc42b65b697
Sean Bills <seanmichaelbills@gmail.com>
2019-08-19 20:50:38 -0400

    updated to use CRUD json server

--- BlogDemoApp/src/context/BlogContext.js
+++ BlogDemoApp/src/context/BlogContext.js
@@ -3,13 +3,13 @@ import jsonServer from '../api/jsonServer'
 
 const blogReducer = (state, action) => {
     switch (action.type) {
-        case 'add_blog_post':
-            return [
-                ...state, {
-                title: action.payload.title,
-                content: action.payload.content,
-                id: Math.floor(Math.random() * 999999999),
-            }]
+        // case 'add_blog_post':
+        //     return [
+        //         ...state, {
+        //         title: action.payload.title,
+        //         content: action.payload.content,
+        //         id: Math.floor(Math.random() * 999999999),
+        //     }]
         case 'delete_blog_post':
             return state.filter((blogPost) => blogPost.id !== action.payload)
         case 'update_blog_post':
@@ -31,28 +31,38 @@ const getBlogPosts = dispatch => {
 }
 
 const addBlogPost = (dispatch) => {
-    return (title, content, callback) => {
-        dispatch(
-            {
-                type: 'add_blog_post',
-                payload: {
-                    title,
-                    content
-                }
-            }
-        )
+    return async (title, content, callback) => {
+        await jsonServer.post("/blogposts", {
+            title,
+            content
+        })
+        // dispatch(
+        //     {
+        //         type: 'add_blog_post',
+        //         payload: {
+        //             title,
+        //             content
+        //         }
+        //     }
+        // )
         callback()
     }
 }
 
 const deleteBlogPost = (dispatch) => {
-    return (id) => {
+    return async (id) => {
+        await jsonServer.delete(`/blogposts/${id}`)
         dispatch({type: 'delete_blog_post', payload: id})
     }
 }
 
 const updateBlogPost = (dispatch) => {
-    return (id, title, content, callback) => {
+    return async (id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`,
+        {
+            title, content
+        })
+        
         dispatch(
             {
                 type: 'update_blog_post',
