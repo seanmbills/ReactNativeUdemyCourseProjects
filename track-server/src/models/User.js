276df7296f35d8bc441e7292257ff60a1bd57649
Sean Bills <seanmichaelbills@gmail.com>
2019-08-19 23:39:57 -0400

    signing up and signing in with password and email

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,45 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// pre-save hook: function that runs before saving instance of user in database
+userSchema.pre('save', function(next) {
+    const user = this
+    // if user hasn't modified password in any way, don't salt anything
+    if (!user.isModified('password')) {
+        return next()
+    }
+    // 10 digit salt
+    bcrypt.genSalt(10, (err, salt) => {
+        // if error generating salt, break out
+        if (err) {
+            return next(err)
+        }
+        // else hash the password and reset user password to the hash
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err)
+            }
+            user.password = hash
+        })
+    })
+})
+
+userSchema.methods.comparePassword = function(enteredPassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(enteredPassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err)
+            }
+
+            if (!isMatch) {
+                return reject(false)
+            }
+
+            resolve(true)
+        })
+    })
+}
 
 // tells mongoose what schema to use for the User object
 mongoose.model('User', userSchema)
