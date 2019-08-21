b2dcee13f1d051e99f09a1cb94000d94ff035cd7
Sean Bills <seanmichaelbills@gmail.com>
2019-08-20 21:44:28 -0400

    created spacer object for adding margin on elements

--- TrackRunnerDemoApp/src/screens/SignupScreen.js
+++ TrackRunnerDemoApp/src/screens/SignupScreen.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import {View, StyleSheet} from 'react-native'
 import {Text, Button, Input} from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({navigation}) => {
     return (
-        <View>
-            {/* <Text style={{fontSize: 48}}>SignupScreen</Text>
-            <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')}/>
-            <Button title="Go to Main flow" onPress={() => navigation.navigate('mainFlow')}/> */}
-            <Text h3>Sign Up for Tracker</Text>
-            <Input label="Email"/>
-            <Input label="Password"/>
-            <Button title="Sign Up"/>
-        </View>
+        <>
+            <View>
+                {/* <Text style={{fontSize: 48}}>SignupScreen</Text>
+                <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')}/>
+                <Button title="Go to Main flow" onPress={() => navigation.navigate('mainFlow')}/> */}
+                <Spacer>
+                    <Text h3>Sign Up for Tracker</Text>
+                </Spacer>
+                <Input label="Email"/>
+                <Spacer />
+                <Input label="Password"/>
+                <Spacer>
+                    <Button title="Sign Up"/>
+                </Spacer>
+            </View>
+        </>
     )
 }
 
