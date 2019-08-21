63448c49b7e6dc00f7e9071192c01a5b5ff5a561
Sean Bills <seanmichaelbills@gmail.com>
2019-08-21 17:25:50 -0400

    setting up software to use axios to send requests to our express api and thus mongodb

--- TrackRunnerDemoApp/src/context/AuthContext.js
+++ TrackRunnerDemoApp/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
     switch(action.type){
@@ -7,8 +8,34 @@ const authReducer = (state, action) => {
     }
 }
 
+const signup = (dispatch) => {
+    return ({email, password}) => {
+        // make api request to sign up with this email and password
+
+        // if we sign up, probably want to modify state and say we are authenticated
+
+        // if signing up fails, we probably need to reflect an error message somewhere
+    }
+}
+
+const signin = (dispatch) => {
+    return ({email, password}) => {
+        // make api request to sign in with given email and password
+
+        // handle success by updating state
+
+        // handle error by showing error message somehow
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        // just need to sign out (revoke authorization of token?)
+    }
+}
+
 export const {Provider, Context} = createDataContext(
     authReducer,
-    {},
+    {signin, signup, signout},
     {isSignedIn: false}
 )
